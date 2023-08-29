df3a99d89484c0648056fbce0a4f75da0def8855
Joseph M. Temesi <joemunyendo@gmail.com>
2023-08-29 09:31:40 +0300

    A js script that estimates a dog owner's age given the age of the dog

---
+++ 03_ JAVASCRIPT/14.3 JS- Basic arithmetics/index.js
@@ -0,0 +1,9 @@
+// Dog age to human age converter
+
+//humanAge = (dogAge - 2) * 4 + 21
+
+var dogAge = prompt ("Enter the age of the dog: ");
+
+var humanAge = (dogAge - 2) * 4 + 21;
+
+alert("If the dog's age is " + dogAge + " then the human's age is " + humanAge);
