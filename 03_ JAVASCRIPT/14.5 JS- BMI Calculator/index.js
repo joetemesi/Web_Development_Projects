dee33dad547a42801d6d6d68c53608f304b70b80
Joseph M. Temesi <joemunyendo@gmail.com>
2023-08-31 20:26:56 +0300

    Added comments

--- 03_ JAVASCRIPT/14.5 JS- BMI Calculator/index.js
+++ 03_ JAVASCRIPT/14.5 JS- BMI Calculator/index.js
@@ -1,11 +1,15 @@
+/*
+bmi = weight / (height * height)
+*/
+
 function getBmi (weight, height) {
     const bmi = bmiCalculate(weight, height);
     alert("Your BMI is " + bmi);
-  }
+  } //calls functions that contains bmi calclation formula and outputs the BMI  as an alert
 
   
 function bmiCalculate (weight, height) {
-    return weight/Math.pow(height, 2); 
-  }
+    return weight/Math.pow(height, 2); //method to get square
+  } //Function that calculates bmi
 
-getBmi(parseFloat(prompt("Enter weight in kg")), parseFloat(prompt("Enter height in metres")));
+getBmi(parseFloat(prompt("Enter weight in kg")), parseFloat(prompt("Enter height in metres"))); // gives arguments to the function via a prompt
