bafe105dbb03f8d98c45feec4758ab1cbd5199d6
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-01 09:43:39 +0300

    Added Rounding off method to output

--- 03_ JAVASCRIPT/14.5 JS- BMI Calculator/index.js
+++ 03_ JAVASCRIPT/14.5 JS- BMI Calculator/index.js
@@ -3,13 +3,13 @@ bmi = weight / (height * height)
 */
 
 function getBmi (weight, height) {
-    const bmi = bmiCalculate(weight, height);
-    alert("Your BMI is " + bmi);
-  } //calls function that contains bmi calculation formula and outputs the BMI  as an alert
+  var bmi = bmiCalculate(weight, height);
+  alert("Your BMI is " + Math.round(bmi));
+} //calls functions that contains bmi calclation formula and outputs the BMI  as an alert
 
-  
+
 function bmiCalculate (weight, height) {
-    return weight/Math.pow(height, 2); //method to get square
-  } //Function that calculates bmi
+  return weight/Math.pow(height, 2); //method to get square
+} //Function that calculates bmi
 
 getBmi(parseFloat(prompt("Enter weight in kg")), parseFloat(prompt("Enter height in metres"))); // gives arguments to the function via a prompt
