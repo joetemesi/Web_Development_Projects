17074cf9acc655208c3fe81abb5df940e363d708
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-03 09:58:12 +0300

    Added comments

--- 03_ JAVASCRIPT/15.1 JS- Leap Year/index.js
+++ 03_ JAVASCRIPT/15.1 JS- Leap Year/index.js
@@ -1,13 +1,13 @@
 function isLeapYr (year) {
-    if ((year % 4) !== 0) {
+    if ((year % 4) !== 0) { //Checks whether year is perfectly divisible by 4
         alert("The year " + year + " is NOT a leap year.");
-    } else if ((year % 100) !== 0){
+    } else if ((year % 100) !== 0){ //Checks whether year is perfectly divisible by 100
         alert("The year " + year + " is a leap year.");
-    } else if ((year % 400) !== 0 ) {
+    } else if ((year % 400) !== 0 ) { //Checks whether year is perfectly divisible by 400
         alert("The year " + year + " is NOT a leap year.");
     } else {
        alert("The year " + year + " is a leap year."); 
     }      
-}
+} //Checks whether a given year is leap or not
     
-isLeapYr(prompt("Enter the year"));
+isLeapYr(prompt("Enter the year")); 
