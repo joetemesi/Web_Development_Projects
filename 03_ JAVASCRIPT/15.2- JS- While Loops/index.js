f787b5f7b9f2ee21b9e297af2f009ac9b2986d6c
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-03 16:39:52 +0300

    Added conditional statement and extra lyrics

--- 03_ JAVASCRIPT/15.2- JS- While Loops/index.js
+++ 03_ JAVASCRIPT/15.2- JS- While Loops/index.js
@@ -2,7 +2,7 @@ var beer = 99;
 var beerLeft = 98;
 
 function myBeer(beer, beerLeft) {
-    while (beer > 0 && beerLeft >= 0) { //loops until no of beers and bottles > 1
+    while (beer > 0 && beerLeft >= 0) { //loops until no of beers and bottles < 1
         if (beerLeft < 1) {
             console.log(beer + " bottles of beer, " + beer + " bottles of beer.");
             console.log(beer + " bottles of beer, Take 1 down, pass it around, no more bottles of beer on the wall.");   
