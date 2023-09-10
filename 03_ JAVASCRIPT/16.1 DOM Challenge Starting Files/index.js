c6d4e71922400064f16ef3c838bbf7c85a2db116
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-10 15:21:59 +0300

    Modified background color of button using DOM query selector

--- 03_ JAVASCRIPT/16.1 DOM Challenge Starting Files/index.js
+++ 03_ JAVASCRIPT/16.1 DOM Challenge Starting Files/index.js
@@ -1,4 +1,6 @@
 document.query
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "TEMESI"
 
-document.querySelector("li a").style.color = "red"
+document.querySelector("li a").style.color = "red";
+
+document.querySelector("Button").style.backgroundColor="yellow";
