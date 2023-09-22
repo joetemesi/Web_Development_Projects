e918804cd28f25d7d9660382b618927f02a8d2b9
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-22 15:56:32 +0300

    Added calculator function

--- 03_ JAVASCRIPT/18.2 Calculator/index.js
+++ 03_ JAVASCRIPT/18.2 Calculator/index.js
@@ -13,3 +13,11 @@ function multiplication (num1,num2) {
 function division (num1,num2) {
     return num1 / num2;
 }
+
+function calculator (num1,num2,operator) {
+    var result = operator(num1,num2);
+    result = parseFloat(result.toFixed(2));
+    return result;
+}
+
+calculator (2,3.5,multiplication);
