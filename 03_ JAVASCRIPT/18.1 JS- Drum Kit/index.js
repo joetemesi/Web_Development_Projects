79625c4c469aae0ce12c75d6539177c28ab1a428
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-23 11:07:38 +0300

    Added sound to button click

--- 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
+++ 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
@@ -2,7 +2,9 @@ var drumClicks = document.querySelectorAll(".drum");
 
 for (var i = 0; i < drumClicks.length; i++) {
     drumClicks[i].addEventListener("click", function () {
-        alert("Key Press.");
+        //alert("Key Press.");
+        var sound1 = new Audio ("sounds/tom-1.mp3");
+        sound1.play();
     });
 }
 
