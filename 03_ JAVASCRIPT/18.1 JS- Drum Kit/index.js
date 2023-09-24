ae3fb4e1c6eee2c722c8d5d309173b3746798e3a
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-24 14:57:49 +0300

    Integrated button click and keyboard press event listeners to make sounds

--- 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
+++ 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
@@ -1,4 +1,4 @@
-var drumClicks = document.querySelectorAll(".drum");
+var drumClicks = document.querySelectorAll(".drum");c// button click event listener.
 
 for (var i = 0; i < drumClicks.length; i++) {
     drumClicks[i].addEventListener("click", function test () {
@@ -7,77 +7,49 @@ for (var i = 0; i < drumClicks.length; i++) {
         sound1.play();
         this.style.color = "white";*/
         var buttonInnerHtml1 = this.innerHTML;
-        switch (buttonInnerHtml1) {
-            case "w":
-                var tom1 = new Audio ("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case "a":
-                var tom2 = new Audio ("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case "s":
-                var tom3 = new Audio ("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio ("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case "j":
-                var tom5 = new Audio ("sounds/snare.mp3");
-                tom5.play();
-                break;
-            case "k":
-                var tom6 = new Audio ("sounds/crash.mp3");
-                tom6.play();
-                break;
-            case "l":
-                var tom7 = new Audio ("sounds/kick-bass.mp3");
-                tom7.play();
-                break;
-
-            default: console.log(buttonInnerHtml1);
-                break;
-        }
-    });
-}
-
-function testKey () {
-    document.addEventListener("keypress", function(event) {
-        var keyPress1 = event.key;
-        switch (keyPress1) {
-            case "w":
-                var tom1 = new Audio ("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-                case "a":
-                    var tom2 = new Audio ("sounds/tom-2.mp3");
-                    tom2.play();
-                    break;
-                case "s":
-                    var tom3 = new Audio ("sounds/tom-3.mp3");
-                    tom3.play();
-                    break;
-                case "d":
-                    var tom4 = new Audio ("sounds/tom-4.mp3");
-                    tom4.play();
-                    break;
-                case "j":
-                    var tom5 = new Audio ("sounds/snare.mp3");
-                    tom5.play();
-                    break;
-                case "k":
-                    var tom6 = new Audio ("sounds/crash.mp3");
-                    tom6.play();
-                    break;
-                case "l":
-                    var tom7 = new Audio ("sounds/kick-bass.mp3");
-                    tom7.play();
-                    break;
-            default: console.log(keyPress1);S
-        }
+        makeSound(buttonInnerHtml1);
     })
-}
+} 
 
-testKey();
+
+ document.addEventListener("keypress", function (event) { ///keyboard press event listeners
+    var keyPress2 = event.key;
+    makeSound(keyPress2);
+    })
+
+
+function makeSound (key) { ///Drum kit sounds
+    switch (key) {
+        case "w":
+            var tom1 = new Audio ("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio ("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio ("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio ("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var tom5 = new Audio ("sounds/snare.mp3");
+            tom5.play();
+            break;
+        case "k":
+            var tom6 = new Audio ("sounds/crash.mp3");
+            tom6.play();
+            break;
+        case "l":
+            var tom7 = new Audio ("sounds/kick-bass.mp3");
+            tom7.play();
+            break;
+
+        default: console.log(buttonInnerHtml1);
+            break;
+    }
+ }
