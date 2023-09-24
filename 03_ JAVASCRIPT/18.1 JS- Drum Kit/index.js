f67f3595576df04f55c73af043f87ff9337239b9
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-24 10:53:35 +0300

    Added switch case statement for first button press

--- 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
+++ 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
@@ -3,9 +3,19 @@ var drumClicks = document.querySelectorAll(".drum");
 for (var i = 0; i < drumClicks.length; i++) {
     drumClicks[i].addEventListener("click", function () {
         //alert("Key Press.");
-        var sound1 = new Audio ("sounds/tom-1.mp3");
+        /*var sound1 = new Audio ("sounds/tom-1.mp3");
         sound1.play();
-        this.style.color = "white";
+        this.style.color = "white";*/
+        var buttonInnerHtml1 = this.innerHTML;
+        switch (buttonInnerHtml1) {
+            case "w":
+                var tom1 = new Audio ("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+        
+            default:
+                break;
+        }
     });
 }
 
