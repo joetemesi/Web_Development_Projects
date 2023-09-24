e8019625f49c571c28e0b384c1015e12ab7f338b
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-24 14:25:55 +0300

    Created a function that makes sounds based on the key pressed

--- 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
+++ 03_ JAVASCRIPT/18.1 JS- Drum Kit/index.js
@@ -23,15 +23,19 @@ for (var i = 0; i < drumClicks.length; i++) {
             case "d":
                 var tom4 = new Audio ("sounds/tom-4.mp3");
                 tom4.play();
+                break;
             case "j":
                 var tom5 = new Audio ("sounds/snare.mp3");
                 tom5.play();
+                break;
             case "k":
                 var tom6 = new Audio ("sounds/crash.mp3");
                 tom6.play();
+                break;
             case "l":
                 var tom7 = new Audio ("sounds/kick-bass.mp3");
                 tom7.play();
+                break;
 
             default: console.log(buttonInnerHtml1);
                 break;
@@ -42,8 +46,36 @@ for (var i = 0; i < drumClicks.length; i++) {
 function testKey () {
     document.addEventListener("keypress", function(event) {
         var keyPress1 = event.key;
-        if (keyPress1 = "w"){
-            alert("test");
+        switch (keyPress1) {
+            case "w":
+                var tom1 = new Audio ("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+                case "a":
+                    var tom2 = new Audio ("sounds/tom-2.mp3");
+                    tom2.play();
+                    break;
+                case "s":
+                    var tom3 = new Audio ("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+                case "d":
+                    var tom4 = new Audio ("sounds/tom-4.mp3");
+                    tom4.play();
+                    break;
+                case "j":
+                    var tom5 = new Audio ("sounds/snare.mp3");
+                    tom5.play();
+                    break;
+                case "k":
+                    var tom6 = new Audio ("sounds/crash.mp3");
+                    tom6.play();
+                    break;
+                case "l":
+                    var tom7 = new Audio ("sounds/kick-bass.mp3");
+                    tom7.play();
+                    break;
+            default: console.log(keyPress1);S
         }
     })
 }
