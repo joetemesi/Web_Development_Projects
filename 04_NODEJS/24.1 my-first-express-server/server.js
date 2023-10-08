630fad3142fdc8781b502ec43f58123c6f8ec80c
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-08 07:12:56 +0300

    Handled requests and get responses on server

--- 04_NODEJS/24.1 my-first-express-server/server.js
+++ 04_NODEJS/24.1 my-first-express-server/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, res){ //Handling requests and GET responses
+    console.log(req); 
+    res.send("Hello World");
+})
+
 app.listen(8054, function(){
     console.log("Server started on port: 8054");
 })
