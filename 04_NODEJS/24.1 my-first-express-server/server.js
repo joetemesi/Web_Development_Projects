c80b1353c81a7cdae5fa1953cec75ffe405f2326
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-09 09:45:39 +0300

    Added about-me route

--- 04_NODEJS/24.1 my-first-express-server/server.js
+++ 04_NODEJS/24.1 my-first-express-server/server.js
@@ -7,6 +7,11 @@ app.get("/", function(req, res){ //Handling requests and GET responses
     res.send("Hello World");
 })
 
+app.get("/about-me", function(req, res){
+    console.log(req);
+    res.send("<h1>Hey there, my name is Joseph Temesi and this is my first express server.</h2>");
+})
+
 app.listen(8054, function(){
     console.log("Server started on port: 8054");
 })
