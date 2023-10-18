f719335238f033be7c49f0e13d0566ed71bab03b
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-18 12:27:33 +0300

    Added form to send POST rquest to server

--- 04_NODEJS/25.2 WeatherProject/app.js
+++ 04_NODEJS/25.2 WeatherProject/app.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const express = require("express");
 
 const app = express();
@@ -8,41 +9,47 @@ const https = require("https");
 
 
 app.get("/", function(req, res){
-    const appKey='';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=mandera&units=metric&appid=${appKey}`;
 
-    https.get(url, function(response) {
-        // console.log(response);
-        console.log(response.statusCode);
+    res.sendFile(__dirname + "/index.html");
+    // const appKey='';
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=mandera&units=metric&appid=${appKey}`;
 
-        response.on("data", function(data){
+    // https.get(url, function(response) {
+    //     // console.log(response);
+    //     console.log(response.statusCode);
+
+    //     response.on("data", function(data){
             
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description
-            const city = weatherData.name;
-            const country = weatherData.sys.country;
-            const icon = weatherData.weather[0].icon;
+    //         const weatherData = JSON.parse(data);
+    //         const temp = weatherData.main.temp;
+    //         const description = weatherData.weather[0].description
+    //         const city = weatherData.name;
+    //         const country = weatherData.sys.country;
+    //         const icon = weatherData.weather[0].icon;
 
     
 
-            console.log(description);
-            console.log(temp);
-            // console.log(weatherData);
-            // res.send(`
-            // <h1>The temperature of ${city} in ${country} is ${temp} degree Celcius</h1>
-            // <h2>The weather is currently ${description}</h2>
-            // `);
+    //         console.log(description);
+    //         console.log(temp);
+    //         // console.log(weatherData);
+    //         // res.send(`
+    //         // <h1>The temperature of ${city} in ${country} is ${temp} degree Celcius</h1>
+    //         // <h2>The weather is currently ${description}</h2>
+    //         // `);
 
-            res.write(`<p>The weather is currently ${description}</p>`);
-            res.write(`<h1>The temperature of ${city} in ${country} is ${temp} Degrees Celcius </h1>`);
-            res.write(`<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`);
-            res.send();
-        })
-    })
+    //         res.write(`<p>The weather is currently ${description}</p>`);
+    //         res.write(`<h1>The temperature of ${city} in ${country} is ${temp} Degrees Celcius </h1>`);
+    //         res.write(`<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`);
+    //         res.send();
+    //     })
+    // })
     
 
     
+})
+
+app.post("/response", function(req, res){
+    console.log("response gotten");
 })
 
 
